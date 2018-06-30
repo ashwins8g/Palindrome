b6834f2e4131ff708ddfa7ee8e6444adf63ca3ef
Ashwin George <39828923+ashwins8g@users.noreply.github.com>
2018-06-30 12:43:17 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+public class NumberPalindrome {
+
+    public static boolean isPalindrome(int inputNumber) {
+
+        String stringToInt = Integer.toString(Math.abs(inputNumber));
+
+        StringBuilder reversedString = new StringBuilder(stringToInt);
+
+        reversedString.reverse();
+
+        if (stringToInt.equals(reversedString.toString())) {
+
+            return true;
+        }
+
+        return false;
+    
+}
+
+}
